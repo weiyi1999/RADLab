37b826b6416109836d81fba655b9f66683370c65
Weiyi <s3692028@student.rmit.edu.au>
2020-05-01 13:41:37 +1000

    Tute 7 production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+    attr_accessor :remember_token
+
     before_save {self.email=email.downcase}
     validates :name, presence: true, length:{maximum:50}
     validates :email, presence: true, length:{maximum:255}
@@ -14,4 +17,22 @@ class User < ApplicationRecord
         BCrypt::Password.create(string,csot: cost)
     end
     
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token=User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest,nil)
+    end
+
 end
