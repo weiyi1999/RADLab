16eaf6bd159b6378c2e8bfe3cc45ada0ff1a15c5
Weiyi <s3692028@student.rmit.edu.au>
2020-05-28 23:37:40 +1000

    email activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors=true
-  config.action_mailer.delivery_method=:smtphost='damp-bayou-44035.herokuapp.com'
+  config.action_mailer.delivery_method=:smtp
+  host='damp-bayou-44035.herokuapp.com'
   config.action_mailer.default_url_options={host: host }
   ActionMailer::Base.smtp_settings=
   {:address=>'smtp.sendgrid.net',
