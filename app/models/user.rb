2857adf2d3cebc7197b2c74fd8fabc6d5f8af60e
Weiyi <s3692028@student.rmit.edu.au>
2020-05-29 15:57:11 +1000

    reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     before_create :create_activation_digest
     before_save {self.email=email.downcase}
@@ -48,6 +48,19 @@ class User < ApplicationRecord
     end
     
 
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
 
     private
         def create_activation_digest
